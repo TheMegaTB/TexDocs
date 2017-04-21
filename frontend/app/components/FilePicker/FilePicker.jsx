78d86b8c26292c5d51ca8a44114ba38329ae6773
Til Blechschmidt <til@blechschmidt.de>
2017-04-21 10:43:19 +0200

    Added sticky header to home

--- frontend/app/components/FilePicker/FilePicker.jsx
+++ frontend/app/components/FilePicker/FilePicker.jsx
@@ -9,6 +9,13 @@ import './FilePicker.css';
 import {Link} from "react-router-dom";
 import Measure from "react-measure";
 import {dateToString} from "../../api/time";
+import {
+    DropDownMenu, FontIcon, IconMenu, MenuItem, RaisedButton, Subheader, Toolbar, ToolbarGroup, ToolbarSeparator,
+    ToolbarTitle
+} from "material-ui";
+import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more';
+import {Sticky, StickyContainer} from "react-sticky";
+
 
 pdfjsLib.PDFJS.workerSrc = '/pdf.worker.js';
 
@@ -23,11 +30,11 @@ const styles = {
         display: 'flex',
         flexWrap: 'wrap',
         justifyContent: 'space-around',
-        height: 'calc(100% - 74px)', // 64px (document metadata height) + PICKER_DOC_MARGIN
-        overflowY: 'auto',
-        padding: PICKER_DOC_MARGIN * 2,
+        // height: 'calc(100% - 74px)', // 64px (document metadata height) + PICKER_DOC_MARGIN
+        // overflowY: 'auto',
     },
     container: {
+        padding: `0 ${PICKER_DOC_MARGIN * 2}`,
         backgroundColor: '#eee',
         maxWidth: (PICKER_DOC_WIDTH + PICKER_DOC_BORDER * 2 + PICKER_DOC_MARGIN * 2) * PICKER_MAX_COLUMNS + PICKER_PADDING,
         margin: 'auto'
@@ -98,45 +105,70 @@ export default class FilePicker extends Component {
     render() {
         const gridDimensions = {
             width: this.state.cols * (PICKER_DOC_WIDTH + PICKER_DOC_MARGIN * 2),
-            height: Math.ceil(this.state.files.length / this.state.cols) * (PICKER_DOC_HEIGHT + PICKER_DOC_MARGIN * 2)
+            height: Math.ceil(this.state.files.length / this.state.cols) * (PICKER_DOC_HEIGHT + PICKER_DOC_MARGIN * 2) + PICKER_PADDING
         };
 
         // if (gridDimensions.height > this.state.height) gridDimensions.height = this.state.height - this.state.cols * PICKER_DOC_MARGIN * 2;
 
         return (
-            <div style={styles.container}>
-                <Measure onMeasure={(dimensions) => {this.setState({ height: dimensions.height, cols: Math.floor(dimensions.width / PICKER_DOC_WIDTH) })}}>
-                    <div style={styles.root}>
-                        <GridList
-                            cellHeight='auto'
-                            cols={this.state.cols}
-                            style={{
-                                width: gridDimensions.width,
-                                height: gridDimensions.height
-                            }}
-                        >
-                            {this.state.files.map((file) => (
-                                <GridTile
-                                    key={file.id}
-                                    containerElement={<Link to={`/d/${file.id}`} />}
-                                    title={file.name}
-                                    style={{width: PICKER_DOC_WIDTH, height: PICKER_DOC_HEIGHT, color: 'black'}}
-                                    titleBackground="white"
-                                    titleStyle={{color: 'black'}}
-                                    subtitle={`Opened ${dateToString(new Date(file.viewedByMeTime))}`}
-                                    className="picker-document"
-                                    actionIcon={
-                                        <IconButton><MoreIcon color="#9E9E9E" hoverColor="#424242"/></IconButton>
-                                    }
-                                >
-                                    <img src={this.state.thumbnails[file.id]}/>
-                                </GridTile>
-                                )
-                            )}
-                        </GridList>
-                    </div>
-                </Measure>
-            </div>
+            <StickyContainer>
+                <Sticky style={{width: '100%'}} className="picker-toolbar">
+                    <Toolbar style={{backgroundColor: '#eee', width: gridDimensions.width, margin: 'auto', paddingRight: 0}}>
+                        <ToolbarGroup firstChild={true}>
+                            <Subheader>Recent documents</Subheader>
+                        </ToolbarGroup>
+                        <ToolbarGroup>
+                            <ToolbarTitle text="Options" />
+                            <FontIcon className="muidocs-icon-custom-sort" />
+                            <ToolbarSeparator />
+                            <RaisedButton label="Create Broadcast" primary={true} />
+                            <IconMenu
+                                iconButtonElement={
+                                    <IconButton touch={true}>
+                                        <NavigationExpandMoreIcon />
+                                    </IconButton>
+                                }
+                            >
+                                <MenuItem primaryText="Download" />
+                                <MenuItem primaryText="More Info" />
+                            </IconMenu>
+                        </ToolbarGroup>
+                    </Toolbar>
+                </Sticky>
+                <div style={styles.container}>
+                    <Measure onMeasure={(dimensions) => {this.setState({ height: dimensions.height, cols: Math.floor(dimensions.width / PICKER_DOC_WIDTH) })}}>
+                        <div style={styles.root}>
+                            <GridList
+                                cellHeight='auto'
+                                cols={this.state.cols}
+                                style={{
+                                    width: gridDimensions.width,
+                                    height: gridDimensions.height
+                                }}
+                            >
+                                {this.state.files.map((file) => (
+                                    <GridTile
+                                        key={file.id}
+                                        containerElement={<Link to={`/d/${file.id}`} />}
+                                        title={file.name}
+                                        style={{width: PICKER_DOC_WIDTH, height: PICKER_DOC_HEIGHT, color: 'black'}}
+                                        titleBackground="white"
+                                        titleStyle={{color: 'black'}}
+                                        subtitle={`Opened ${dateToString(new Date(file.viewedByMeTime))}`}
+                                        className="picker-document"
+                                        actionIcon={
+                                            <IconButton><MoreIcon color="#9E9E9E" hoverColor="#424242"/></IconButton>
+                                        }
+                                    >
+                                        <img src={this.state.thumbnails[file.id]}/>
+                                    </GridTile>
+                                    )
+                                )}
+                            </GridList>
+                        </div>
+                    </Measure>
+                </div>
+            </StickyContainer>
         )
     }
 }
--- frontend/app/components/Home/Home.jsx
+++ frontend/app/components/Home/Home.jsx
@@ -4,6 +4,7 @@ import FilePicker from "../FilePicker/FilePicker";
 import {AppBar} from "material-ui";
 
 import './Home.css';
+import {Sticky, StickyContainer} from "react-sticky";
 
 export default class Home extends React.Component {
     constructor(props) {
@@ -11,15 +12,22 @@ export default class Home extends React.Component {
     }
 
     render() {
-        return <div>
-            <AppBar
-                title="TexDocs"
-                style={{backgroundColor: '#FF5722'}}
-            />
-            <div className="picker-container">
-                <FilePicker/>
+        return <StickyContainer style={{backgroundColor: '#eee'}}>
+            <Sticky onStickyStateChange={(e) => {console.log(e)}}>
+                <AppBar
+                    title="TexDocs"
+                    style={{backgroundColor: '#FF5722'}}
+                />
+            </Sticky>
+            <div className="content">
+                <div className="template-picker">
+                    Hey there!
+                </div>
+                <div className="picker-container">
+                    <FilePicker/>
+                </div>
             </div>
             {/*<NoDocumentPopup history={this.props.history} title="No document opened." text="Choose one of the following options to get started."/>*/}
-        </div>;
+        </StickyContainer>;
     }
 }
