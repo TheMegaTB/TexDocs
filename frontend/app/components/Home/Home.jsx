adb0204acc43a06067700827a95202dd956ccd3c
Til Blechschmidt <til@blechschmidt.de>
2017-06-05 22:52:28 +0200

    Added ability to create files

--- frontend/app/components/Home/Home.jsx
+++ frontend/app/components/Home/Home.jsx
@@ -1,31 +1,42 @@
-import React from "react";
-import FilePicker from "../FilePicker/FilePicker";
-import {AppBar} from "material-ui";
+import React from 'react';
+import { connect } from 'react-redux';
+import { AppBar } from 'material-ui';
+import { Sticky, StickyContainer } from 'react-sticky';
+import FilePicker from '../FilePicker/FilePicker';
+import { createFile } from '../../redux/actions/navigation.js';
 
 import './Home.css';
-import {Sticky, StickyContainer} from "react-sticky";
 
-export default class Home extends React.Component {
-    constructor(props) {
-        super(props);
+class Home extends React.Component {
+
+    createAndOpenFile = () => {
+        this.props.dispatch(createFile(this.props.googleAPI.get('api').drive));
     }
 
     render() {
-        return <StickyContainer style={{backgroundColor: '#eee'}}>
+        return (<StickyContainer style={{ backgroundColor: '#eee' }}>
             <Sticky>
                 <AppBar
                     title="TexDocs"
-                    style={{backgroundColor: '#FF5722'}}
+                    style={{ backgroundColor: '#FF5722' }}
                 />
             </Sticky>
             <div className="content">
                 <div className="template-picker">
-                    Hey there!
+                    <button onClick={this.createAndOpenFile}>Click to create new document</button>
                 </div>
                 <div className="picker-container">
-                    <FilePicker/>
+                    <FilePicker />
                 </div>
             </div>
-        </StickyContainer>;
+        </StickyContainer>);
     }
 }
+
+Home.propTypes = {
+    googleAPI: React.PropTypes.object.isRequired
+};
+
+export default connect(
+(state) => { return { googleAPI: state.googleAPI }; }
+)(Home);
